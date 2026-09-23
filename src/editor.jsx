01d3b66ee3deb5df0c96fed22d5b6dfa0fda50df
Rich Tabor <rich@tabor.email>
2026-09-22 20:17:29 -0400

    Hide floating Canvas inserter on mobile

--- src/editor.jsx
+++ src/editor.jsx
@@ -1344,6 +1344,7 @@ export default function Edit( { clientId, attributes, isSelected } ) {
 				/>
 			</InspectorControls>
 			{ ! preview &&
+				mode !== 'mobile' &&
 				!! insertion.allowed.length &&
 				( isSelected || !! selectedId ) && (
 					<CanvasInserter
